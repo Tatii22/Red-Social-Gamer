2e70dfdf6a1b553316c8082fddcdaa42790cc9c4
Dayana <dayanita1204bb@gmail.com>
2025-07-06 11:46:49 -0600

    feat: :sparkles: Se crea la funcion crear publicacion

---
+++ src/publicaciones.py
@@ -0,0 +1,22 @@
+import json 
+import questionary
+from rich.console import Console
+
+# Consola Rich
+console = Console()
+# Crear una nueva publicación
+def crear_publicacion(usuario):
+    console.rule("[bold cyan]🎮 Publicar Contenido Gamer")
+
+    tipo = questionary.select(
+        "🖥️ ¿Qué tipo de contenido vas a publicar?",
+        choices=["logro", "noticia", "captura", "recomendación"]
+    ).ask()
+
+    contenido = questionary.text("📸 Escribe tu publicación:").ask()
+
+    if not contenido.strip():
+        console.print("[bold red]❌ No se puede publicar contenido vacío.[/bold red]")
+        return
+    
+    
