0b5e41609bb461aef0fcf9337c380c6876d53477
Tatiana Villamizar <tm8497077@gmail.com>
2025-07-05 19:49:45 -0500

    Feat: :sparkles: Se crea la funcion de registro de archivo

--- src/usuarios.py
+++ src/usuarios.py
@@ -3,6 +3,7 @@ import os
 import questionary
 from rich.console import Console
 from rich.prompt import Prompt
+from estilos import gamerStyle
 
 console = Console()
 RUTA_USUARIOS = os.path.join("data", "usuarios.json")
@@ -24,4 +25,23 @@ def guardarUsuarios(usuarios):
     with open(RUTA_USUARIOS, "w", encoding="utf-8") as archivo:
         json.dump(usuarios, archivo, indent=4, ensure_ascii=False)
 
+def registrarUsuario():
+    usuarios = cargarUsuarios()
 
+    console.print("\n[bold cyan]🎮 Registro de nuevo jugador[/bold cyan]\n")
+    nombreUser = questionary.text("👤 Nombre de usuario:", style=gamerStyle).ask().strip()
+    if any(user["nombreUser"] == nombreUser for user in usuarios):
+        console.print("❌ [bold red]El nombre de usuario ya existe.[/bold red]")
+        return
+
+    password = questionary.password("🔒 Contraseña:", style=gamerStyle).ask().strip()
+
+    nuevo_usuario = {
+        "nombreUser": nombreUser,
+        "password": password
+    }
+
+    usuarios.append(nuevo_usuario)
+    guardarUsuarios(usuarios)
+
+    console.print(f"\n✅ [bold green]Usuario '{nombreUser}' registrado con éxito.[/bold green]")
